29e7c1b7854531bfb631aa351d8f19b933826706
kokoro-aya <35118912+kokoro-aya@users.noreply.github.com>
2023-12-22 01:56:38 +0100

    Added main function entrypoint
    Use argparse to parse and switch to different APIs

--- dog_breed.py
+++ dog_breed.py
@@ -4,6 +4,7 @@ from typing import Final, List
 import random
 import urllib.request
 import json
+import argparse
 
 REMOTE_API_BASE: Final[str] = "https://dog.ceo/api/"
 ENDPOINT_LIST_ALL: Final[str] = "breeds/list/all"
@@ -49,3 +50,38 @@ def download_image(breed_name: str, filename: str, img_no: int = None):
         print("There is no such breed in the database, skipping ...")
     else:
         __save_to_local_file(img_url, filename)
+
+if __name__ == '__main__':
+    arg_parser = argparse.ArgumentParser(
+        prog="dog-breeds",
+        description="a simple client for some dog-api endpoints",
+        epilog="----------"
+    )
+    
+    # push the command to `action` key stored in parsed args to know which sub parser is called
+    sub_parsers = arg_parser.add_subparsers(dest="action")
+
+    sub_list_all_parser = sub_parsers.add_parser("list-all", help="List all breeds")
+
+    sub_get_image_parser = sub_parsers.add_parser("get-image", help="Get image of a breed, required arguments: --breed and --file")
+    sub_get_image_parser.add_argument("-b", "--breed", required=True)
+    sub_get_image_parser.add_argument("-f", "--file", required=True)
+
+    args = vars(arg_parser.parse_args())
+
+    # there should always be an "action" pair
+
+    # Case 1: no command is called, print the help menu and quit
+    if args["action"] is None:
+        arg_parser.print_help()
+
+    # Case 2: `list-all` command
+    if args["action"] == "list-all":
+        list_all()
+
+    # Case 3: `get-image` command
+    if args["action"] == "get-image":
+        # Retrieve arguments (they are required)
+        arg_breed = args["breed"]
+        arg_filepath = args["file"]
+        download_image(arg_breed, arg_filepath, None)
